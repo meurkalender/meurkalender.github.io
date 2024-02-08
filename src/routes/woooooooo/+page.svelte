74875602bbfdc978c13f2fefa9d6a58589b28b6c
sbalk <stijn.balk@gmail.com>
2024-02-08 22:34:56 +0100

    success text toegevoegd

--- src/routes/woooooooo/+page.svelte
+++ src/routes/woooooooo/+page.svelte
@@ -5,6 +5,7 @@
 <div class="container">
 	<img class="image" src="{base}/images/woooo.gif" alt="" />
 </div>
+<p>Succes! Je Meurkalender is besteld. En je geld onderweg naar een Zwitserse Bank.</p>
 
 <style>
 	.container {
@@ -12,6 +13,23 @@
 		top: 50%;
 		left: 50%;
 		transform: translate(-50%, -50%);
+	}
+	p {
+		height: 90vh;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		/* align-items: center; */
+		/* margin: auto;
+		width: 50%;
+		justify-content: center;
+		padding: 10px; */
+		/* display: flex;
+		text-align: center;
+		align-items: center; */
+		font-family: 'Prompt', sans-serif;
+		font-size: 2em;
+		color: #b6c9e9;
 	}
 
 	@media only screen and (max-width: 700px) {
@@ -21,6 +39,9 @@
 		.container {
 			width: 90vw;
 			padding: 10;
+		}
+		p {
+			font-size: 1em;
 		}
 	}
 </style>
