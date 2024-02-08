e5acf8ce0a75a4d32b6cf35a2d3659a4f385b4c6
sbalk <stijn.balk@gmail.com>
2024-02-08 22:40:59 +0100

    update sveltekit config

--- svelte.config.js
+++ svelte.config.js
@@ -10,8 +10,8 @@ const config = {
 		adapter: adapter(),
 
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-			// base: process.env.NODE_ENV === 'production' ? '/meurkalender' : ''
+			// base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.env.NODE_ENV === 'production' ? '/meurkalender' : ''
 		}
 	}
 };
