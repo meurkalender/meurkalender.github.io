6827418fb59ee7cbabda886f8538d253f3fefe68
sbalk <stijn.balk@gmail.com>
2024-02-08 19:43:12 +0100

    added successful payout

---
+++ src/routes/woooooooo/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
+<div class="container">
+	<img src="{base}/images/woooo.gif" alt="" />
+</div>
+
+<style>
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		padding: 0;
+	}
+</style>
