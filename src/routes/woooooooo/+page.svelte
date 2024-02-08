365c33b820f94c9f150853c0dc6df03e6fdc24fa
sbalk <stijn.balk@gmail.com>
2024-02-08 19:56:09 +0100

    added successful white

--- src/routes/woooooooo/+page.svelte
+++ src/routes/woooooooo/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="container">
-	<img src="{base}/images/woooo.gif" alt="" />
+	<img class="image" src="{base}/images/woooo.gif" alt="" />
 </div>
 
 <style>
@@ -11,7 +11,19 @@
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		height: 100vh;
 		padding: 0;
+	}
+
+	@media only screen and (max-width: 700px) {
+		.image {
+			width: 90vw;
+		}
+		.container {
+			width: 90vw;
+			padding: 10;
+		}
+		background {
+			background-color: white;
+		}
 	}
 </style>
